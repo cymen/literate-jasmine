f7c34f56d8350bb8dcc68967174a24f0272a41fb
Cymen Vig <cymenvig@gmail.com>
2014-02-17 00:21:21 -0600

    Integration tests are not working properly on Travis CI -- move to 'npm run integration'

--- spec/integration.js
+++ spec/integration.js
@@ -5,16 +5,16 @@ var assert = require('assert'),
 sinon.spy(console, 'log');
 
 (function referenceError() {
-  try {
-    process.argv[2] = __dirname + '/markdown/reference.md';
-    require('../bin/literate-jasmine');
-  }
-  catch (e) {
-    // swallow it so CI works
-  }
+  console.log('NOTE: looks like failure but this is integration test -- failure expected, watch for assertion failure!'.yellow);
+
+  process.argv[2] = __dirname + '/markdown/reference.md';
+  require('../bin/literate-jasmine');
 
   setTimeout(function() {
     assert(console.log.calledWith('ReferenceError: zz is not defined'.red, 'thrown from', 'c'.red + ':'));
     assert(console.log.calledWith('c = 2 * zz;'.red));
+
+    console.log('NOTE: looks like failure but this is integration test -- failure expected, watch for assertion failure!'.yellow);
+    console.log('\n');
   }, 250);
 })();
